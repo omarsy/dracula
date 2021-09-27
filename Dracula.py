dc5640a8e264953fc0302e60be65d667f44416a7
Omar SY <syomar.pro@gmail.com>
2021-09-27 19:05:30 +0200

    use support and resistance

--- Dracula.py
+++ Dracula.py
@@ -28,7 +28,7 @@ import numpy as np
 ##   Prefer stable coin (USDT, BUSDT etc) pairs, instead of BTC or ETH pairs.                            ##
 ##   Highly recommended to blacklist leveraged tokens (*BULL, *BEAR, *UP, *DOWN etc).                    ##
 ##   Ensure that you don't override any variables in you config.json. Especially                         ##
-##   the timeframe (must be 1m).                                                                         ##
+##   the timeframe (should be 5m).                                                                         ##
 ##                                                                                                       ##
 ###########################################################################################################
 ###########################################################################################################
@@ -36,19 +36,19 @@ import numpy as np
 ##                                                                                                       ##
 ##                                                                                                       ##
 ###########################################################################################################
+
 class Dracula(IStrategy):
 
     # Buy hyperspace params:
     buy_params = {
-        "buy_rsi_protection_rsi_max": 25,
+        "buy_rsi_protection_rsi_max": 30,
         "buy_btc_lost_protection": True,
-        "buy_use_ich": False,
         "buy_use_bb": True,
     }
 
     # Sell hyperspace params:
     sell_params = {
-        "sell_rsi_limit": 80,
+        "sell_rsi_limit": 70,
     }
     # ROI table:
     minimal_roi = {
@@ -57,7 +57,7 @@ class Dracula(IStrategy):
 
     info_timeframe = "5m"
     # Stoploss:
-    stoploss = -0.3
+    stoploss = -0.2
     min_lost = -0.005
 
     buy_btc_lost_protection = CategoricalParameter([True, False], default=True, space='buy', optimize=False, load=True)
@@ -71,22 +71,29 @@ class Dracula(IStrategy):
     # Buy hypers
     timeframe = '1m'
     
+    # Trailing stoploss (not used)
+    trailing_stop = False
+    trailing_only_offset_is_reached = True
+    trailing_stop_positive = 0.01
+    trailing_stop_positive_offset = 0.1
     custom_info = {}
 
     
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
-        dataframe['bb_bbh_i'] = ta.volatility.bollinger_hband_indicator(close=dataframe["close"], window=20)
-        dataframe['bb_bbl_i'] = ta.volatility.bollinger_lband_indicator(close=dataframe["close"], window=20)
-        dataframe['bb_bbh'] = ta.volatility.bollinger_hband(close=dataframe["close"], window=20)
-        dataframe['bb_bbl'] = ta.volatility.bollinger_lband(close=dataframe["close"], window=20)
-
+        dataframe['bb_bbh_i'] = ta.volatility.bollinger_hband_indicator(close=dataframe["close"], window=15)
+        dataframe['bb_bbl_i'] = ta.volatility.bollinger_lband_indicator(close=dataframe["close"], window=15)
+        dataframe['bb_bbh'] = ta.volatility.bollinger_hband(close=dataframe["close"], window=15)
+        dataframe['bb_bbl'] = ta.volatility.bollinger_lband(close=dataframe["close"], window=15)
+        dataframe['bb_bbt'] = (dataframe['bb_bbh'] - dataframe['bb_bbl']) / dataframe['bb_bbh']
         dataframe['rsi'] = taa.RSI(dataframe, timeperiod=14)
 
         dataframe['healthy'] = dataframe['close']  > dataframe['close'].shift(120)
         btc_info_tf = self.dp.get_pair_dataframe("BTC/BUSD", timeframe=self.timeframe)
+        dataframe['btc_open'] = btc_info_tf['open']
+        dataframe['btc_close'] = btc_info_tf['close']
         btc_info_tf['rsi'] = taa.RSI(btc_info_tf, timeperiod=14)
-        btc_shift = btc_info_tf['close'].shift(10)
+        btc_shift = btc_info_tf['close'].shift(2)
         dataframe['btc_lost'] = (btc_info_tf['close'] - btc_shift) / pd.concat([btc_shift, btc_info_tf['close']]).max(level=0)
         dataframe['btc_healthy'] = (dataframe['btc_lost'] > self.min_lost)
         
@@ -98,13 +105,15 @@ class Dracula(IStrategy):
         conditions.append(dataframe['volume'] > 0)
         conditions.append(dataframe['bb_bbl_i'].shift(1) == 1)
         conditions.append((dataframe['open'] < dataframe['close']))
+        conditions.append((dataframe['close'] - dataframe['open']) > ((dataframe['open'].shift(1) - dataframe['close'].shift(1)) * 0.7))
+        conditions.append((dataframe['bb_bbt'] > 0.02))
         # if not last_candle['healthy']: 
         #     conditions.append(dataframe["rsi"] < 25)
         #conditions.append(dataframe['bb_bbh_i'].rolling(min_periods=1, window=5).sum() > 1)
         # if self.buy_rsi_protection.value:
         #     conditions.append(dataframe["rsi"] < self.buy_rsi_protection_rsi_max.value)
-        # if self.buy_btc_lost_protection.value:
-        #     conditions.append(dataframe['btc_healthy'])
+        if self.buy_btc_lost_protection.value:
+            conditions.append((dataframe['btc_open'] < dataframe['btc_close']))
 
         if conditions:
             dataframe.loc[
@@ -118,9 +127,11 @@ class Dracula(IStrategy):
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         conditions = []
         item_sell_logic = []
-        item_sell_logic.append(dataframe['bb_bbh_i'] == 1)
+        item_sell_logic.append(dataframe['bb_bbh_i'].shift(1) == 1)
+        #item_sell_logic.append(dataframe['bb_bbh_i'] == 0)
         #if last_candle['healthy']: 
-        item_sell_logic.append((dataframe["rsi"] >= self.sell_rsi_limit.value))
+        # item_sell_logic.append((dataframe["rsi"] >= self.sell_rsi_limit.value))
+        item_sell_logic.append(dataframe['close'] < dataframe['open'])
         item_sell_logic.append(dataframe['volume'] > 0)
         conditions.append(reduce(lambda x, y: x & y, item_sell_logic))
         
